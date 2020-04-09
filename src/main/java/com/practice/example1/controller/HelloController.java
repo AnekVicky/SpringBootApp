ddf346e1f0a7ee14492a144cad66cc3101067dd7
AnekVicky <anekkumarsingh01@gmail.com>
2020-04-09 20:28:39 +0530

    Added In Memory Authentication

--- src/main/java/com/practice/example1/controller/HelloController.java
+++ src/main/java/com/practice/example1/controller/HelloController.java
@@ -1,16 +1,16 @@
 package com.practice.example1.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String sayHi(){
 
-        return "Hey there , I have come ";
+        return "Hey there ,you have logged in ";
     }
 
 }
